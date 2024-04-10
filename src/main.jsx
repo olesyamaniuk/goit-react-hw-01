87dd8c9858212ff3b160d7935276ebb1bf981711
olesyamaniuk <maniukolesya222@gmail.com>
2024-04-10 14:55:23 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './compinents/App.jsx'
+import App from './components/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
